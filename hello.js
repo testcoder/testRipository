a57556629fabbfb344405f7ebee8f36190e599c6
testcoder <o855561@rtrtr.com>
2012-10-01 17:00:39 +0900

    Update hello.js
    実に無意味なif文を追加。

--- hello.js
+++ hello.js
@@ -1,4 +1,9 @@
 body.onload = function() {
 	console.log("Hello,World!!");
 	console.log("Hello,GitHub!!");
+	if ( 1 + 3 === 0.504 ) {
+		document.write("んなわけねえだろ(^^)");
+	}　else {
+		document.write("GitHubのエディタ高性能過ぎワロタ");
+	}
 }
